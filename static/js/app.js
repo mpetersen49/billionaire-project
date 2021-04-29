96af18181994554cd747528050a9ac7d07cfe392
Jon Weber <jonweber0328@gmail.com>
2021-04-29 11:50:20 -0500

    Added WebGLEarth and JS library for header animation.

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,28 @@
-console.log("app.js loaded");
+// console.log("app.js loaded");
+
+// Code taken directly from: https://tobiasahlin.com/moving-letters/#2
+// This code will animate h3 text in the header image
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+// End of header text animation 
+
+
