9fccf9bad01add27248761497788672ff6cc4d6d
mpetersen49 <mpetersen49@gmail.com>
2021-05-01 14:03:40 -0500

    Working on app.py code

--- static/py/app.py
+++ static/py/app.py
@@ -1,6 +1,6 @@
 # Import the functions we need from flask
 from flask import Flask
-from flask import render_template 
+# from flask import render_template 
 from flask import jsonify
 
 # Import the functions we need from SQL Alchemy
@@ -9,9 +9,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-connection_string = "sqlite:///../data/billionaires.sqlite"
-
-engine = create_engine(connection_string)
+engine = create_engine("sqlite:///../data/billionaires.sqlite")
 base = automap_base()
 base.prepare(engine, reflect=True)
 
@@ -22,34 +20,37 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/")
 def IndexRoute():
-    return "test"
+    return (
+        f"Available routes:<br>"
+        f"/data"
+    )
 
-@app.route("/data")
-def APIrequest():
-    session = Session(engine)
-    results = session.query(table.Name, 
-                            table.NetWorth, 
-                            table.Country, 
-                            table.Source, 
-                            table.Rank, 
-                            table.latitude, 
-                            table.longitude).all()
+# @app.route("/data")
+# def APIrequest():
+#     session = Session(engine)
+#     results = session.query(table.Name, 
+#                             table.NetWorth, 
+#                             table.Country, 
+#                             table.Source, 
+#                             table.Rank, 
+#                             table.latitude, 
+#                             table.longitude).all()
     
-    session.close()
+#     session.close()
 
-    api_data = []
-    for name, networth, country, source, rank, latitude, longitude in results:
-        dict = {}
-        dict["Name"] = name
-        dict["NetWorth"] = NetWorth
-        dict["Country"] = country
-        dict["Source"] = source
-        dict["Rank"] = rank
-        dict["latitude"] = latitude
-        dict["longitude"] = longitude
-        api_data.append(dict)
+#     api_data = []
+#     for name, networth, country, source, rank, latitude, longitude in results:
+#         dict = {}
+#         dict["Name"] = name
+#         dict["NetWorth"] = NetWorth
+#         dict["Country"] = country
+#         dict["Source"] = source
+#         dict["Rank"] = rank
+#         dict["latitude"] = latitude
+#         dict["longitude"] = longitude
+#         api_data.append(dict)
 
-    return jsonify(api_data)
+#     return jsonify(api_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
